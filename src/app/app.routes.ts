d8d76f972dd21af51ee128bc8d46ae1824b25857
unknown <aoua.sow1@odc.local>
2024-08-14 18:52:30 +0000

    aoua

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,38 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'splash-screen',
+    loadComponent: () => import('./splash-screen/splash-screen.page').then( m => m.SplashScreenPage)
   },
-  
+
   {
     path: 'accueil',
     loadComponent: () => import('./pages/accueil/accueil.page').then( m => m.AccueilPage)
   },
   {
     path: '',
-    redirectTo: 'accueil',
+    redirectTo: 'splash-screen',
     pathMatch: 'full',
   },
   {
     path: 'profil',
     loadComponent: () => import('./pages/profil/profil.page').then( m => m.ProfilPage)
   },
-
+  {
+    path: 'home1',
+    loadComponent: () => import('./home1/home1.page').then( m => m.Home1Page)
+  },
+  {
+    path: 'chargement',
+    loadComponent: () => import('./chargement/chargement.page').then( m => m.ChargementPage)
+  },
+  {
+    path: 'chargement1',
+    loadComponent: () => import('./chargement1/chargement1.page').then( m => m.Chargement1Page)
+  },
+  {
+    path: 'chargement2',
+    loadComponent: () => import('./chargement2/chargement2.page').then( m => m.Chargement2Page)
+  },
 
 ];
