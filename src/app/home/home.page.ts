b465f9851e5c925e818274fd898fd4e5ffd46a56
adama <serminadama2@gmail.com>
2024-08-15 10:11:12 +0000

    Finition de la partie visuelle

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,7 +12,31 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 export class HomePage {
 
   mosque: String = "assets/images/djenne-mosque 1.png";
-
-
+  pays: any =[ {
+    "name": "Mali",
+    "capital": "Bamako",
+    "prix": 1500
+  },
+  {
+    "name": "Burkina Faso",
+    "capital": "Ouaga",
+    "prix": 2000
+  },
+  {
+    "name": "Côte d'Ivoire",
+    "capital": "Abidjan",
+    "prix": 2500
+  },
+  {
+    "name": "Sénégal",
+    "capital": "Dakar",
+    "prix": 3000
+  },
+  {
+    "name": "Guinée",
+    "capital": "Conakry",
+    "prix": 3500
+  }
+  ]
   constructor() {}
 }
