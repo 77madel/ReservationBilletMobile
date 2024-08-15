0e0d684de866eccefb9f25d38ddc8bd031937788
adama <serminadama2@gmail.com>
2024-08-15 14:38:24 +0000

    Partie visuelle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -38,6 +38,9 @@ export const routes: Routes = [
   {
     path: 'ticket',
     loadComponent: () => import('./pages/Tickets/ticket/ticket.page').then( m => m.TicketPage)
+    
+  },
+  {
     path: 'home1',
     loadComponent: () => import('./home1/home1.page').then( m => m.Home1Page)
   },
@@ -53,7 +56,7 @@ export const routes: Routes = [
     path: 'chargement2',
     loadComponent: () => import('./chargement2/chargement2.page').then( m => m.Chargement2Page)
   },
-
+  {
     path: 'profil',
     loadComponent: () => import('./pages/profil/profil.page').then( m => m.ProfilPage)
   },
