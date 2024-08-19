d9a110c63dd2e2cdd819c9c5549906ac0d0da42f
unknown <mamoutou.yalcouye@odc.local>
2024-08-19 16:43:12 +0000

    lundi

--- src/app/services/login-service.service.ts
+++ src/app/services/login-service.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, catchError, map, Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,38 +10,45 @@ import { BehaviorSubject, catchError, map, Observable } from 'rxjs';
 export class LoginServiceService {
 
   private isLoggedIn = false;
-    private apiUrl = 'http://localhost:8080/personne/signin'; // URL de votre API Spring Boot
-    
-    private currentUserSubject!: BehaviorSubject<any>;
-    public currentUser!: Observable<any>;    static jwtToken: any;
-  
-    constructor(private http: HttpClient,private router: Router) {}
-  
-    login(username: string, password: string): Observable<any> {
-      const credentials = { username, password };
-      return this.http.post<any>(`${this.apiUrl}`, credentials).pipe(
-        map(user => {
-          if (user && user.jwtToken) {
-            this.isLoggedIn=true;
-            localStorage.setItem('currentUser', JSON.stringify(user));
+  private apiUrl = 'http://localhost:8080/personne/signin';
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
 
-          }
-          return user;
-
-          
-        }),
-      );
-    }
-  
-    logout(username: string='', password: string=''): void {
-      localStorage.removeItem('currentUser');
-      this.router.navigate(["/login"])
-      this.isLoggedIn=false;
-      
-    }
-
-    verifLoggedIn():boolean{
-      return this.isLoggedIn;
-    }
-    
+  constructor(private http: HttpClient, private router: Router) {
+    const storedUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
+    this.currentUserSubject = new BehaviorSubject<any>(storedUser);
+    this.currentUser = this.currentUserSubject.asObservable();
   }
+
+  login(username: string, password: string): Observable<any> {
+    const credentials = { username, password };
+    return this.http.post<any>(`${this.apiUrl}`, credentials).pipe(
+      map(user => {
+        console.log('Utilisateur connecté:', user); // Ajouter cette ligne pour vérifier l'objet utilisateur
+        if (user && user.jwtToken) {
+          this.isLoggedIn = true;
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user); // Mettre à jour le BehaviorSubject
+        }
+        return user;
+      })
+
+    );
+  }
+
+  logout(): void {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null); // Réinitialiser le BehaviorSubject
+    this.router.navigate(['/login']);
+    this.isLoggedIn = false;
+  }
+
+  verifLoggedIn(): boolean {
+    return this.isLoggedIn;
+  }
+
+  getUserId(): number | null {
+    const user = this.currentUserSubject.value;
+    return user && user.id ? user.id : null;
+  }
+}
